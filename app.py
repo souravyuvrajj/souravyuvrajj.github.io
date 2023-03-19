3843cf7949d4e1b6d8f306e5a13ed71e65909998
sourabh singh <souravyuvrajj@gmail.com>
2023-03-19 23:53:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
 
@@ -7,5 +8,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    freezer.freeze()
